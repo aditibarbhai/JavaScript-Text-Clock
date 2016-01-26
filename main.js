62020a0d6409b12bb92e8de4e9d267c3615fb2b3
Aditi <aditi@Aditis-MacBook-Pro.local>
2016-01-25 19:57:12 -0500

    Improving main.js code to be more generic, less if cases

--- main.js
+++ main.js
@@ -2,8 +2,26 @@ function getTime () {
 	return new Date();
 }
 
+var minsInWords = {
+	5 : "five",
+	10 : "ten",
+	15 : "quarter",
+	20 : "twenty",
+	25 : ["twenty", "five"],
+	30 : "half",
+	35 : ["twenty", "five"],
+	40 : "twenty",
+	45 : "quarter",
+	50 : "ten",
+	55 : "five"
+};
+
+function roundToFive (num) {
+	return 5 * Math.round(num/5);
+}
+
 function setTime () {
-	$(".clock .changing").children().removeClass("selected");
+	$(".clock .changing").children().removeClass("selected", { easing: 'easeOutBounce' });
 
 	var currTime = getTime();
 	var hours = currTime.getHours();
@@ -21,173 +39,195 @@ function setTime () {
 	// set the hour 
 	if (mins <= 30) {
 		$("#" + hours).addClass("selected");
+		$("#past").addClass("selected");
 	} else {
 		$("#" + (hours + 1)).addClass("selected");
+		$("#to").addClass("selected");
 	}
 
-	// set the minutes
-	if (mins == 0) {
+	if (mins % 5 == 0) {
 		// do nothing
-	} else if (mins < 3) {
+	} else if (mins % 5 < 3) {
 		$("#around").addClass("selected");
-	} else if (mins >= 3 && mins < 5) {
-		$("#almost").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins == 5) {
-		$("#minutes").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins < 7) {
-		$("#around").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins >= 7 && mins < 10) {
-		$("#almost").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#tenM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins == 10) {
-		$("#minutes").addClass("selected");
-		$("#tenM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins < 13) {
-		$("#around").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#tenM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins >= 13 && mins < 15) {
-		$("#almost").addClass("selected");
-		$("#quarter").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins == 15) {
-		$("#quarter").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins < 17) {
-		$("#around").addClass("selected");
-		$("#quarter").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins >= 17 && mins < 20) {
-		$("#almost").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins == 20) {
-		$("#minutes").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins < 23) {
-		$("#around").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins >= 23 && mins < 25) {
-		$("#almost").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins == 25) {
-		$("#twenty").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins < 27) {
-		$("#around").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins >= 27 && mins < 30) {
-		$("#almost").addClass("selected");
-		$("#half").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins == 30) {
-		$("#half").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins < 33) {
-		$("#around").addClass("selected");
-		$("#half").addClass("selected");
-		$("#past").addClass("selected");
-	} else if (mins >= 33 && mins < 35) {
-		$("#almost").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins == 35) {
-		$("#twenty").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins < 37) {
-		$("#around").addClass("selected");
-		$("#minutes").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins >= 37 && mins < 40) {
-		$("#minutes").addClass("selected");
-		$("#almost").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins == 40) {
-		$("#minutes").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins < 43) {
-		$("#minutes").addClass("selected");
-		$("#around").addClass("selected");
-		$("#twenty").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins >= 43 && mins < 45) {
-		$("#almost").addClass("selected");
-		$("#quarter").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins == 45) {
-		$("#quarter").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins < 47) {
-		$("#around").addClass("selected");
-		$("#quarter").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins >= 47 && mins < 50) {
-		$("#minutes").addClass("selected");
-		$("#almost").addClass("selected");
-		$("#tenM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins == 50) {
-		$("#minutes").addClass("selected");
-		$("#tenM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins < 53) {
-		$("#minutes").addClass("selected");
-		$("#around").addClass("selected");
-		$("#tenM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins >= 53 && mins < 55) {
-		$("#minutes").addClass("selected");
-		$("#almost").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins == 55) {
-		$("#minutes").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins < 57) {
-		$("#minutes").addClass("selected");
-		$("#around").addClass("selected");
-		$("#fiveM").addClass("selected");
-		$("#to").addClass("selected");
-	} else if (mins >= 57 && mins <= 59) {
+	} else if (mins % 5 <= 4) {
 		$("#almost").addClass("selected");
 	}
 
 	$("#oclock").addClass("selected");
+
+	// set the minutes
+	if (mins > 4) {
+		var roundFive = roundToFive(mins);
+		var minsWords = minsInWords[roundFive];
+		if (Array.isArray(minsWords)) {
+			$.each(minsWords, function(i, el) {
+				$("#" + el).addClass("selected");
+			});
+		} else {
+			$("#" + minsWords).addClass("selected");
+		}
+	}
+
+	// if (mins == 0) {
+	// 	// do nothing
+	// } else if (mins < 3) {
+	// 	$("#around").addClass("selected");
+	// } else if (mins >= 3 && mins < 5) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins == 5) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins < 7) {
+	// 	$("#around").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins >= 7 && mins < 10) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#tenM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins == 10) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#tenM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins < 13) {
+	// 	$("#around").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#tenM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins >= 13 && mins < 15) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#quarter").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins == 15) {
+	// 	$("#quarter").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins < 17) {
+	// 	$("#around").addClass("selected");
+	// 	$("#quarter").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins >= 17 && mins < 20) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins == 20) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins < 23) {
+	// 	$("#around").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins >= 23 && mins < 25) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins == 25) {
+	// 	$("#twenty").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins < 27) {
+	// 	$("#around").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins >= 27 && mins < 30) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#half").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins == 30) {
+	// 	$("#half").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins < 33) {
+	// 	$("#around").addClass("selected");
+	// 	$("#half").addClass("selected");
+	// 	$("#past").addClass("selected");
+	// } else if (mins >= 33 && mins < 35) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins == 35) {
+	// 	$("#twenty").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins < 37) {
+	// 	$("#around").addClass("selected");
+	// 	$("#minutes").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins >= 37 && mins < 40) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#almost").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins == 40) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins < 43) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#around").addClass("selected");
+	// 	$("#twenty").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins >= 43 && mins < 45) {
+	// 	$("#almost").addClass("selected");
+	// 	$("#quarter").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins == 45) {
+	// 	$("#quarter").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins < 47) {
+	// 	$("#around").addClass("selected");
+	// 	$("#quarter").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins >= 47 && mins < 50) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#almost").addClass("selected");
+	// 	$("#tenM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins == 50) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#tenM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins < 53) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#around").addClass("selected");
+	// 	$("#tenM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins >= 53 && mins < 55) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#almost").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins == 55) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins < 57) {
+	// 	$("#minutes").addClass("selected");
+	// 	$("#around").addClass("selected");
+	// 	$("#fiveM").addClass("selected");
+	// 	$("#to").addClass("selected");
+	// } else if (mins >= 57 && mins <= 59) {
+	// 	$("#almost").addClass("selected");
+	// }
 }
 
 $(document).ready(function(){
 	setTime();
-	setInterval(function(){ setTime(); }, 5000);
+	setInterval(function(){ setTime(); }, 3000);
 });
